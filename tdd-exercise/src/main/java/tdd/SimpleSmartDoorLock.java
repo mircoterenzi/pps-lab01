9f9d7c3fe0f72fbd8fc74bcd3896ee149afbc6cd
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2025-02-24 14:04:02 +0100

    add second exercise - step 1

---
+++ tdd-exercise/src/main/java/tdd/SimpleSmartDoorLock.java
@@ -0,0 +1,77 @@
+package tdd;
+
+public class SimpleSmartDoorLock implements SmartDoorLock {
+    private static final int PIN_NOT_SET = -1;
+    private static final int PIN_LENGTH = 4;
+    private static final int MAX_ATTEMPTS = 3;
+
+    private int pin, failedAttempts;
+    private boolean locked, blocked;
+
+    private void initializeVariables() {
+        this.blocked = false;
+        this.locked = false;
+        this.failedAttempts = 0;
+        this.pin = PIN_NOT_SET;
+    }
+
+    public SimpleSmartDoorLock() {
+        initializeVariables();
+    }
+
+    @Override
+    public void setPin(int pin) {
+        if (blocked || locked) {
+            throw new IllegalStateException("Cannot set pin if system isn't unlocked");
+        }
+        if (String.valueOf(pin).length() != PIN_LENGTH) {
+            throw new IllegalArgumentException("Wrong pin length");
+        }
+        this.pin = pin;
+    }
+
+    @Override
+    public void unlock(int pin) {
+        if (blocked) {
+            throw new IllegalStateException("Cannot unlock while the system is blocked");
+        }
+        if (pin == this.pin) {
+            this.locked = false;
+        } else if (++this.failedAttempts == MAX_ATTEMPTS) {
+            this.blocked = true;
+        }
+    }
+
+    @Override
+    public void lock() {
+        if (this.pin == PIN_NOT_SET) {
+            throw new IllegalStateException("Cannot lock without a pin set");
+        }
+        this.locked = true;
+    }
+
+    @Override
+    public boolean isLocked() {
+        return this.locked;
+    }
+
+    @Override
+    public boolean isBlocked() {
+        return this.blocked;
+    }
+
+    @Override
+    public int getMaxAttempts() {
+        return MAX_ATTEMPTS;
+    }
+
+    @Override
+    public int getFailedAttempts() {
+        return this.failedAttempts;
+    }
+
+    @Override
+    public void reset() {
+        initializeVariables();
+    }
+}
--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -1,12 +1,105 @@
 package tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartDoorLockTest {
+    public static final int PIN = 1234;
+    public static final int WRONGLY_FORMATTED_PIN = 2;
+
+    private SmartDoorLock lock;
+
+    private int getWrongPin() {
+        return PIN + 1;
+    }
+
+    private void setPinAndLock() {
+        this.lock.setPin(PIN);
+        this.lock.lock();
+    }
+
+    private void tryWrongPinUntilBlock() {
+        setPinAndLock();
+        for (int i = 0; i < this.lock.getMaxAttempts(); i++) {
+            this.lock.unlock(getWrongPin());
+        }
+    }
+
+    @BeforeEach
+    public void createSmartDoorLock() {
+        lock = new SimpleSmartDoorLock();
+    }
+
     @Test
-    public void todo() {
-        assertTrue(true);
+    public void testLockWithoutPinSet() {
+        assertThrows(
+                IllegalStateException.class,
+                () -> this.lock.lock()
+        );
+    }
+
+    @Test
+    public void testLock() {
+        setPinAndLock();
+        assertTrue(this.lock.isLocked());
+    }
+
+    @Test void testLockWithPinNotCorrectlyFormatted() {
+        assertThrows(IllegalArgumentException.class, () -> this.lock.setPin(WRONGLY_FORMATTED_PIN));
+    }
+
+    @Test
+    public void testSetPinWhileLocked() {
+        setPinAndLock();
+        assertThrows(IllegalStateException.class, () -> this.lock.setPin(PIN));
+    }
+
+    @Test
+    public void testUnlock() {
+        setPinAndLock();
+        this.lock.unlock(PIN);
+        assertFalse(this.lock.isLocked());
+    }
+
+    @Test
+    public void testUnlockWithWrongPin() {
+        setPinAndLock();
+        this.lock.unlock(getWrongPin());
+        assertAll(
+                () -> assertTrue(this.lock.isLocked()),
+                () -> assertEquals(1, this.lock.getFailedAttempts())
+        );
+    }
+
+    @Test
+    public void testBlock() {
+        tryWrongPinUntilBlock();
+        assertTrue(this.lock.isBlocked());
+    }
+
+    @Test
+    public void testUnlockWhileBlocked() {
+        tryWrongPinUntilBlock();
+        assertThrows(IllegalStateException.class, () -> this.lock.unlock(PIN));
+    }
+
+    @Test
+    public void testSetPinWhileBlocked() {
+        tryWrongPinUntilBlock();
+        assertThrows(IllegalStateException.class, () -> this.lock.setPin(PIN));
+    }
+    
+    @Test
+    public void testReset() {
+        tryWrongPinUntilBlock();
+        this.lock.reset();
+        assertAll(
+                () -> assertFalse(this.lock.isBlocked()),
+                () -> assertFalse(this.lock.isLocked()),
+                () -> assertEquals(0, this.lock.getFailedAttempts()),
+                () -> assertThrows(IllegalStateException.class, () -> this.lock.lock())
+        );
     }
 }
